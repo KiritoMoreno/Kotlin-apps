6b60207e6c78412e209500ad99dab24553fe1803
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-15 18:15:56 +0000

    Creamos nuestra Activity Menu

--- app/src/main/java/com/example/kotlinapps/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/example/kotlinapps/firstapp/FirstAppActivity.kt
@@ -1,16 +1,16 @@
-package com.example.kotlinapps
+package com.example.kotlinapps.firstapp
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.example.kotlinapps.R
 
-class FisrtAppActivity : AppCompatActivity() {
+class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fisrt_app)
+        setContentView(R.layout.activity_first_app)
 
         val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
         val etName = findViewById<AppCompatEditText>(R.id.etName)
@@ -18,7 +18,7 @@ class FisrtAppActivity : AppCompatActivity() {
         btnStart.setOnClickListener {
             val name = etName.text.toString()
         if (name.isNotEmpty()){
-            val intent = Intent(this,ResultActivity::class.java)
+            val intent = Intent(this, ResultActivity::class.java)
             intent.putExtra("EXTRA_NAME",name)
             startActivity(intent)
             }
--- app/src/main/java/com/example/kotlinapps/firstapp/ResultActivity.kt
+++ app/src/main/java/com/example/kotlinapps/firstapp/ResultActivity.kt
@@ -1,8 +1,9 @@
-package com.example.kotlinapps
+package com.example.kotlinapps.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.kotlinapps.R
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
