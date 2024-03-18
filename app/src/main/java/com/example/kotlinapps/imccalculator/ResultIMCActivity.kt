4927c66846050659cc3179481b5273eacfbcd33e
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-18 19:04:51 +0000

    Logica del cardView para la ResultActivity

--- app/src/main/java/com/example/kotlinapps/imccalculator/ResultIMCActivity.kt
+++ app/src/main/java/com/example/kotlinapps/imccalculator/ResultIMCActivity.kt
@@ -2,14 +2,60 @@ package com.example.kotlinapps.imccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.kotlinapps.R
 import com.example.kotlinapps.imccalculator.ImcCalculatorActivity.Companion.IMC_KEY
 
 class ResultIMCActivity : AppCompatActivity() {
+    private lateinit var tvresult: TextView
+    private lateinit var tvIMC: TextView
+    private lateinit var tvDescription:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_imcactivity)
 
-        val result = intent.extras?.getDouble(IMC_KEY)?: -1.0
+        val result:Double = intent.extras?.getDouble(IMC_KEY)?: -1.0
+        initComponents()
+        initUI(result)
+    }
+
+    private fun initUI(result: Double) {
+        tvIMC.text= result.toString()
+        when(result){
+            in 0.00 .. 18.50 ->{ // Bajo peso
+
+                tvresult.text=getString(R.string.title_bajo_peso)
+                tvDescription.text=getString(R.string.descripcion_bajo_peso)
+            }
+            in 18.51 .. 24.99 ->{ // Peso normal
+
+                tvresult.text=getString(R.string.title_peso_normal)
+                tvDescription.text=getString(R.string.descripcion_peso_normal)
+            }
+            in 25.00 .. 29.99 ->{ // Sobrepeso
+
+                tvresult.text=getString(R.string.title_sobrepeso)
+                tvDescription.text=getString(R.string.descripcion_sobrepeso)
+            }
+            in 30.00 .. 99.00 ->{ // Obesidad
+
+                tvresult.text=getString(R.string.title_obesidad)
+                tvDescription.text=getString(R.string.descripcion_obesidad)
+            }
+            else->{ // Error
+                tvIMC.text = getString(R.string.error)
+                tvresult.text = getString(R.string.error)
+                tvDescription.text = getString(R.string.error)
+            }
+        }
+    }
+
+    private fun initComponents(){
+        tvIMC = findViewById(R.id.tvIMC)
+        tvresult= findViewById(R.id.tvResult)
+        tvDescription= findViewById(R.id.tvDescription)
+
     }
 }
