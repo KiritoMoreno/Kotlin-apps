ab47e2325a9fde2adfc82ca7252eff0d7efb3858
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-18 21:29:28 +0000

    Agregamos Sealed class TaskCategory Consegiremos que con esto my RecyclerView. A la hora de mostrar, se enviara un listado de TaskCategory (solamente puede contener los objectos que esten dentro )

--- app/src/main/java/com/example/kotlinapps/todoapp/ToDoActivity.kt
+++ app/src/main/java/com/example/kotlinapps/todoapp/ToDoActivity.kt
@@ -2,11 +2,23 @@ package com.example.kotlinapps.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinapps.R
 
 class ToDoActivity : AppCompatActivity() {
+    private lateinit var rvCategories: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do)
+        initComponents()
+        initUI()
+    }
+
+    private fun initComponents(){
+
+    }
+    private fun initUI(){
+
     }
 }
