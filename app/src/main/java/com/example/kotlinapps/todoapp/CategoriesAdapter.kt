185de0b34ec21dbf30e06a6f16c27bd7e2d5ede0
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-18 21:58:46 +0000

    Modificamos el codigo en categoriesAdapter (Estamos creando un inflador de vistas y necesita un contexto. ya que no tenemos el contexto en la misma clase. El contexto lo puede sacar de cualquier componente le pasamos Parent.context).

--- app/src/main/java/com/example/kotlinapps/todoapp/CategoriesAdapter.kt
+++ app/src/main/java/com/example/kotlinapps/todoapp/CategoriesAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.kotlinapps.todoapp
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinapps.R
 
 class CategoriesAdapter(private var categories: List <TaskCategory>):RecyclerView.Adapter<CategoriesViewHolder> {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
-        TODO("Not yet implemented")
+        val view= LayoutInflater.from(parent.context).inflate(R.layout.item_task_category, parent, false)
+        return CategoriesViewHolder(view)
     }
 
     override fun getItemCount(): Int = categories.size
