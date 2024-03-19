4bcea8a4d596f8f8c940a50765f08f262d92b6bf
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-19 16:57:14 +0000

    Filtro por el taskCategory en el CategoriesViewHolder

--- app/src/main/java/com/example/kotlinapps/todoapp/CategoriesViewHolder.kt
+++ app/src/main/java/com/example/kotlinapps/todoapp/CategoriesViewHolder.kt
@@ -10,7 +10,18 @@ class CategoriesViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val tvCategoryName: TextView = view.findViewById(R.id.tvCategoryName)
     private val divider: View= view.findViewById(R.id.divider)
     fun render(taskCategory: TaskCategory){
-        tvCategoryName.text= "EJEMPLO"
+
+        when(taskCategory){
+            TaskCategory.Business -> {
+                tvCategoryName.text="Negocios"
+            }
+            TaskCategory.Other -> {
+                tvCategoryName.text="Otros"
+            }
+            TaskCategory.Personal -> {
+                tvCategoryName.text="Personal"
+            }
+        }
     }
 
 }
