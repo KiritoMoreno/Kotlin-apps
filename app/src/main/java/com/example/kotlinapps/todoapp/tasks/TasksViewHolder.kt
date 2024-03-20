6075e2d20278bb9b03dba0cdd198a8ebfbc511b1
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-20 17:28:37 +0000

    Creamos el codigo para que tache y no tache el item

--- app/src/main/java/com/example/kotlinapps/todoapp/tasks/TasksViewHolder.kt
+++ app/src/main/java/com/example/kotlinapps/todoapp/tasks/TasksViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinapps.todoapp.tasks
 
+import android.graphics.Paint
 import android.view.View
 import android.widget.CheckBox
 import android.widget.TextView
@@ -11,6 +12,13 @@ class TasksViewHolder (view: View):RecyclerView.ViewHolder(view){
     private val tvTask:TextView = view.findViewById(R.id.tvTask)
     private val cbTask:CheckBox = view.findViewById(R.id.cbTask)
     fun render(task:Task){
+        if (task.isSelected){
+            tvTask.paintFlags = tvTask.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+        }else{
+            tvTask.paintFlags = tvTask.paintFlags and  Paint.STRIKE_THRU_TEXT_FLAG.inv()
+        }
+        cbTask.isChecked = task.isSelected
+        
         tvTask.text=task.name
     }
 
