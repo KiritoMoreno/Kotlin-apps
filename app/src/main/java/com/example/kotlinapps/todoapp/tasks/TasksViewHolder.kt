b167fa7545abf7e7e121731990f2c50c573a19a9
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-20 17:39:19 +0000

    Modificamos el TaskViewHolder para poner colores al checkBox

--- app/src/main/java/com/example/kotlinapps/todoapp/tasks/TasksViewHolder.kt
+++ app/src/main/java/com/example/kotlinapps/todoapp/tasks/TasksViewHolder.kt
@@ -1,12 +1,15 @@
 package com.example.kotlinapps.todoapp.tasks
 
+import android.content.res.ColorStateList
 import android.graphics.Paint
 import android.view.View
 import android.widget.CheckBox
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinapps.R
 import com.example.kotlinapps.todoapp.Task
+import com.example.kotlinapps.todoapp.TaskCategory
 
 class TasksViewHolder (view: View):RecyclerView.ViewHolder(view){
     private val tvTask:TextView = view.findViewById(R.id.tvTask)
@@ -17,9 +20,16 @@ class TasksViewHolder (view: View):RecyclerView.ViewHolder(view){
         }else{
             tvTask.paintFlags = tvTask.paintFlags and  Paint.STRIKE_THRU_TEXT_FLAG.inv()
         }
-        cbTask.isChecked = task.isSelected
-        
         tvTask.text=task.name
+        cbTask.isChecked = task.isSelected
+        val color = when (task.category){
+            TaskCategory.Business -> R.color.todo_business_category
+            TaskCategory.Other -> R.color.todo_other_category
+            TaskCategory.Personal -> R.color.todo_personal_category
+        }
+        cbTask.buttonTintList = ColorStateList.valueOf(
+            ContextCompat.getColor(cbTask.context,color)
+        )
     }
 
 }
