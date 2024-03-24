c241f2ed0b22420792a69eacc3d851cbf75a8ec2
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-24 20:06:32 +0000

    Creamos un nueavo archivo SuperHeroDetailResponse que contiene data class,en el ApiService creamos el get para el (ID) y en creamos nuevas funciones en DetailSuperHeroActivity

--- app/src/main/java/com/example/kotlinapps/superheroapp/ApiService.kt
+++ app/src/main/java/com/example/kotlinapps/superheroapp/ApiService.kt
@@ -1,6 +1,7 @@
 package com.example.kotlinapps.superheroapp
 
 import com.example.kotlinapps.superheroapp.data.SuperHeroDataResponse
+import com.example.kotlinapps.superheroapp.data.SuperHeroDetailResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,5 +10,7 @@ interface ApiService {
     @GET("/api/AQUI FALTA EL TOKEN/search/{name}")
     suspend fun getSuperheroes(@Path("name") superheroName: String): Response<SuperHeroDataResponse>
 
+    @GET("/api/Aqui falta el token/{id}")
+    suspend fun getSuperheroDetails(@Path("id") superheroId : String): Response<SuperHeroDetailResponse>
 
 }
--- app/src/main/java/com/example/kotlinapps/superheroapp/DetailSuperHeroActivity.kt
+++ app/src/main/java/com/example/kotlinapps/superheroapp/DetailSuperHeroActivity.kt
@@ -3,14 +3,52 @@ package com.example.kotlinapps.superheroapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kotlinapps.R
+import com.example.kotlinapps.databinding.ActivityDetailSuperHeroBinding
+import com.example.kotlinapps.superheroapp.data.SuperHeroDetailResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DetailSuperHeroActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityDetailSuperHeroBinding
     companion object{
         const val EXTRA_ID = "extra_id"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_super_hero)
+        binding = ActivityDetailSuperHeroBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val id:String = intent.getStringExtra(EXTRA_ID).orEmpty()
+        getSuperHeroInformation(id)
+    
+    }
+
+    private fun getSuperHeroInformation(id: String) {
+        // Hace la petición a internet (hilo segundario)
+        CoroutineScope(Dispatchers.IO).launch {
+            val superheroDetail =
+                getRetrofit().create(ApiService::class.java).getSuperheroDetails(id) // Solamente hacemos una llamada
+
+            if(superheroDetail.body() !=null){
+                runOnUiThread { createUI(superheroDetail.body()!!) }
+
+            }
+        }
+    }
+    private fun createUI(superhero: SuperHeroDetailResponse){
+
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return  Retrofit
+            .Builder()
+            .baseUrl("https://superheroapi.com/")
+            .addConverterFactory(GsonConverterFactory.create()) // Para Transformar
+            .build()
     }
 }
