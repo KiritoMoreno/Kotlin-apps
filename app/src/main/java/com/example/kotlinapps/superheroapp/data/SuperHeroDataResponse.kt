82627c0bbd8ddf882fd3ff8e4b748f7a30275277
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-24 16:50:32 +0000

    Creamos una nueva data class para poder cargar la imagen y que adentro tiene una URL  y llamamos la imagen  en la data class SuperheroItemResponse

--- app/src/main/java/com/example/kotlinapps/superheroapp/data/SuperHeroDataResponse.kt
+++ app/src/main/java/com/example/kotlinapps/superheroapp/data/SuperHeroDataResponse.kt
@@ -1,14 +1,16 @@
 package com.example.kotlinapps.superheroapp.data
 
 import com.google.gson.annotations.SerializedName
+import java.net.URL
 
-data class SuperHeroDataResponse (@SerializedName("response") val response: String, @SerializedName("results") val superheroes:List<SuperheroItemResponse>){
-
-}
+data class SuperHeroDataResponse(
+    @SerializedName("response") val response: String,
+    @SerializedName("results") val superheroes: List<SuperheroItemResponse>
+)
 
 data class SuperheroItemResponse(
-        @SerializedName("id") val superheroId : String,
-        @SerializedName("name") val name:String
-    ){
-
-}
+    @SerializedName("id") val superheroId: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("image") val superheroImage: SuperHeroImageResponse
+)
+data class SuperHeroImageResponse(@SerializedName("url") val url: String)
