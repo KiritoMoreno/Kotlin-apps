e7c67f73f279abbd4ba7b24a5354d9baeb50e084
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-24 14:09:53 +0000

    Agragamos ViewBinding y lo utilizamos en la app Super_Hero

--- app/src/main/java/com/example/kotlinapps/superheroapp/SuperHeroListActivity.kt
+++ app/src/main/java/com/example/kotlinapps/superheroapp/SuperHeroListActivity.kt
@@ -3,10 +3,15 @@ package com.example.kotlinapps.superheroapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kotlinapps.R
+import com.example.kotlinapps.databinding.ActivitySuperHeroListBinding
 
 class SuperHeroListActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySuperHeroListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_super_hero_list)
+        binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
