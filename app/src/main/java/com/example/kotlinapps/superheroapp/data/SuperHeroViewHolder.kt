2682f503003720922f60329d2f8dc4620d103ded
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-24 17:08:17 +0000

    Actualizamos el diseño de nuestro item_superhero.xml y agregamos la libreria Picasso que nos permite cargar imagenes

--- app/src/main/java/com/example/kotlinapps/superheroapp/data/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/kotlinapps/superheroapp/data/SuperHeroViewHolder.kt
@@ -3,10 +3,14 @@ package com.example.kotlinapps.superheroapp.data
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinapps.databinding.ItemSuperheroBinding
+import com.squareup.picasso.Picasso
 
 class SuperHeroViewHolder (view: View): RecyclerView.ViewHolder(view){
     private val binding = ItemSuperheroBinding.bind(view)
     fun bind(superheroItemResponse: SuperheroItemResponse){
         binding.tvSuperheroName.text = superheroItemResponse.name
+
+        Picasso.get().load(superheroItemResponse.superheroImage.url).into(binding.ivSuperHero)
+        
     }
 }
