fb3d171f6ae39d2b7270f7c3963a4f40cdbbed2f
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-24 16:24:57 +0000

    Agregamos nuevos archivos: SuperHeroAdapter, SuperHeroViewHolder y item_superhero.xml

---
+++ app/src/main/java/com/example/kotlinapps/superheroapp/data/SuperHeroAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinapps.superheroapp.data
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinapps.R
+
+//
+class SuperHeroAdapter(var superheroList: List<SuperheroItemResponse> = emptyList()): RecyclerView.Adapter<SuperHeroViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return SuperHeroViewHolder(layoutInflater.inflate(R.layout.item_superhero,parent,false))
+    }
+
+    override fun onBindViewHolder(viewholder: SuperHeroViewHolder, position: Int) {
+        viewholder.bind(superheroList[position])
+    }
+
+    override fun getItemCount()= superheroList.size
+}
---
+++ app/src/main/java/com/example/kotlinapps/superheroapp/data/SuperHeroViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinapps.superheroapp.data
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SuperHeroViewHolder (view: View): RecyclerView.ViewHolder(view){
+    fun bind(superheroItemResponse: SuperheroItemResponse){}
+}
