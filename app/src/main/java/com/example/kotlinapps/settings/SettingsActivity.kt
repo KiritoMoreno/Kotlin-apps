1fc35e1f1a87b43c44bde8cb77eb54690c2a9853
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-25 20:30:01 +0000

    Creamos las funciones para Guardar los datos

--- app/src/main/java/com/example/kotlinapps/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/kotlinapps/settings/SettingsActivity.kt
@@ -3,23 +3,75 @@ package com.example.kotlinapps.settings
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.kotlinapps.R
 import com.example.kotlinapps.databinding.ActivitySettingsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 //Creamos una función de extension
-val Context.dataStore : DataStore<Preferences>  by preferencesDataStore(name = "settings")
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class SettingsActivity : AppCompatActivity() {
-    private lateinit var  binding:ActivitySettingsBinding
+    companion object {
+        const val VOLUME_LVL = "volume_lvl"
+        const val KEY_BLUETOOTH = "key_bluetooth"
+        const val KEY_DARK_MODE = "key_dark_mode"
+        const val KEY_VIBRATION = "key_vibration"
+    }
+
+    private lateinit var binding: ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
 
-        
 
+    }
+
+    private fun initUI() {
+        binding.rsVolume.addOnChangeListener { _, value, _
+            ->
+            Log.i("Moreno", "El valor es $value")
+            CoroutineScope(Dispatchers.IO).launch { saveVolume(value.toInt()) }
+        }
+
+        binding.switchBluetooth.setOnCheckedChangeListener { _, isChecked ->
+            CoroutineScope(
+                Dispatchers.IO
+            ).launch { saveOptions(KEY_BLUETOOTH, isChecked) }
+        }
+        binding.switchVibrate.setOnCheckedChangeListener { _, isChecked ->
+            CoroutineScope(
+                Dispatchers.IO
+            ).launch { saveOptions(KEY_VIBRATION, isChecked) }
+        }
+        binding.switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
+            CoroutineScope(
+                Dispatchers.IO
+            ).launch { saveOptions(KEY_DARK_MODE, isChecked) }
+        }
+    }
+
+    private suspend fun saveVolume(value: Int) {
+        //Coroutines
+        dataStore.edit { preferences ->
+            preferences[intPreferencesKey(VOLUME_LVL)] = value
+        }
+    }
+
+    private suspend fun saveOptions(key: String, value: Boolean) {
+        // booleano para las opciones de On y Off de los adjustes
+        dataStore.edit { preferences ->
+            preferences[booleanPreferencesKey(key)] = value
+        }
     }
 }
